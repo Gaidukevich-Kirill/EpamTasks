73b22f821027c0e35964ab2c257f48f833c60759
Kirill Gaidukevich <gaidukevich.kirill@gmail.com>
2022-08-19 16:58:43 +0300

    Added unit tests, task image

--- Task1/Task1.Tests/UnitTest1.cs
+++ Task1/Task1.Tests/UnitTest1.cs
@@ -7,6 +7,8 @@ namespace Task1.Tests
     public class Tests
     {
         private Bread _bread;
+        private WhiteBread _whiteBread;
+        private Baguette _baguette;
 
         [SetUp]
         public void Setup()
@@ -20,16 +22,108 @@ namespace Task1.Tests
                     new Salt(10, 10),
                 }
             };
+
+            _whiteBread = new WhiteBread()
+            {
+                Compounds =
+                {
+                    new Flour(40, 40),
+                    new Water(0, 0),
+                    new Salt(10, 10),
+                    new Sugar(20, 20),
+                    new Egg(20, 20),
+                }
+            };
+
+            _baguette = new Baguette()
+            {
+                Compounds =
+                {
+                    new Flour(30, 30),
+                    new Water(0, 0),
+                    new Sugar(30, 30),
+                    new Salt(10, 10),
+                    new Egg(30, 30)
+                }
+            };
         }
 
+        //------------------------------------------Bread------------------------------------
         [Test]
-        public void Test1()
+        public void TestBreadProductPrice()
         {
             // arrange
             int expected = 72;
 
             // act
             var actual = _bread.ProductPrice;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test] 
+        public void TestBreadProductCalories()
+        {
+            // arrange
+            int expected = 110;
+
+            // act
+            var actual = _bread.ProductCalories;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //----------------------------------------------------------WhiteBread---------------------------------
+        [Test]
+        public void TestWhiteBreadProductPrice()
+        {
+            // arrange
+            int expected = 99;
+
+            // act
+            var actual = _whiteBread.ProductPrice;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestWhiteBreadProductCalories()
+        {
+            // arrange
+            int expected = 90;
+
+            // act
+            var actual = _whiteBread.ProductCalories;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //------------------------------------------------------Baguette---------------------------------
+        [Test]
+        public void TestBaguetteProductPrice()
+        {
+            // arrange
+            int expected = 126;
+
+            // act
+            var actual = _baguette.ProductPrice;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestBaguetteProductCalories()
+        {
+            // arrange
+            int expected = 100;
+
+            // act
+            var actual = _baguette.ProductCalories;
 
             // assert
             Assert.AreEqual(expected, actual);
