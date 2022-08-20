a0ef7da2a245e95b645bff627d30f13c6f1e461a
Kirill Gaidukevich <gaidukevich.kirill@gmail.com>
2022-08-20 23:46:37 +0300

    added services new features

--- Task2/Task2/Task2/Program.cs
+++ Task2/Task2/Task2/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using Task2.Products;
 using Task2.Services;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace Task2
 {
@@ -9,8 +11,8 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            var brick1 = new Brick("Brick", 3, 1.23, 5);
-            var brick2 = new Brick("Brick", 5, 1.25, 6);
+            var brick1 = new Brick("Brick Red", 3, 1.23, 5);
+            var brick2 = new Brick("Brick RED", 5, 1.25, 6);
             var brickResult = ServicesByProduct.AdditionOfTwoProducts(brick1, brick2);
 
             List<Brick> listBricks = new List<Brick>();
@@ -20,9 +22,23 @@ namespace Task2
 
             foreach (var item in listBricks)
             {
-                Console.WriteLine($"Purchase Cost: {item.PurchaseCost}, Margin: {item.Margin}, Number of units: {item.NumberOfUnits}" +
+                Console.WriteLine($"Product name: {item.ProductName}, Purchase Cost: {item.PurchaseCost}, " +
+                                  $"Margin: {item.Margin}, Number of units: {item.NumberOfUnits}, " +
+                                  $"Cost by one unit: {item.ProductCostByOneUnit}, Total cost: {item.TotalCost}");
+            }
+
+            var brickResultMinusUnits = ServicesByProduct.SubtractingUnitsOfProduction(brickResult, 3);
+            listBricks.Add(brickResultMinusUnits);
+
+            Console.WriteLine();
+            foreach (var item in listBricks)
+            {
+                Console.WriteLine($"Product name: {item.ProductName}, Purchase Cost: {item.PurchaseCost}, " +
+                                  $"Margin: {item.Margin}, Number of units: {item.NumberOfUnits}, " +
                                   $"Cost by one unit: {item.ProductCostByOneUnit}, Total cost: {item.TotalCost}");
             }
         }
+
+        Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
     }
 }
--- Task2/Task2/Task2/Services/ServicesByProduct.cs
+++ Task2/Task2/Task2/Services/ServicesByProduct.cs
@@ -9,48 +9,135 @@ namespace Task2.Services
     {
         public static Bolt AdditionOfTwoProducts(Bolt leftProduct, Bolt rightProduct)
         {
-            var obj = new Bolt("Bolt", 0, 0, 0)
+            if (leftProduct.ProductName.Equals(rightProduct.ProductName, StringComparison.InvariantCultureIgnoreCase))
             {
-                PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
-                                           + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
-                                          / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
-                Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
-                                     + rightProduct.Margin * rightProduct.NumberOfUnits)
-                                    / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
-                NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
-            };
+                var obj = new Bolt("", 0, 0, 0)
+                {
+                    ProductName = leftProduct.ProductName,
+                    PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
+                                               + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
+                                              / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
+                                         + rightProduct.Margin * rightProduct.NumberOfUnits)
+                                        / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
+                };
 
-            return obj;
+                return obj;
+            }
+            else
+            {
+                throw new Exception("Products have different Product Name");
+            }
         }
 
         public static Brick AdditionOfTwoProducts(Brick leftProduct, Brick rightProduct)
         {
-            var obj = new Brick("Brick", 0, 0, 0)
+            if (leftProduct.ProductName.Equals(rightProduct.ProductName, StringComparison.InvariantCultureIgnoreCase))
             {
-                PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
-                                           + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
-                                          / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
-                Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
-                                     + rightProduct.Margin * rightProduct.NumberOfUnits)
-                                    / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
-                NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
-            };
+                var obj = new Brick("", 0, 0, 0)
+                {
+                    ProductName = leftProduct.ProductName,
+                    PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
+                                               + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
+                                              / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
+                                         + rightProduct.Margin * rightProduct.NumberOfUnits)
+                                        / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
+                };
 
-            return obj;
+                return obj;
+            }
+            else
+            {
+                throw new Exception("Products have different Product Name");
+            }
         }
 
         public static Screw AdditionOfTwoProducts(Screw leftProduct, Screw rightProduct)
         {
-            var obj = new Screw("Screw", 0, 0, 0)
+            if (leftProduct.ProductName.Equals(rightProduct.ProductName, StringComparison.InvariantCultureIgnoreCase))
             {
-                PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
-                                + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
-                               / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits),2),
-                Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
-                          + rightProduct.Margin * rightProduct.NumberOfUnits)
-                         / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
-                NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
+                var obj = new Screw("", 0, 0, 0)
+                {
+                    ProductName = leftProduct.ProductName,
+                    PurchaseCost = Math.Round((leftProduct.PurchaseCost * leftProduct.NumberOfUnits
+                                               + rightProduct.PurchaseCost * rightProduct.NumberOfUnits)
+                                              / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    Margin = Math.Round((leftProduct.Margin * leftProduct.NumberOfUnits
+                                         + rightProduct.Margin * rightProduct.NumberOfUnits)
+                                        / (leftProduct.NumberOfUnits + rightProduct.NumberOfUnits), 2),
+                    NumberOfUnits = leftProduct.NumberOfUnits + rightProduct.NumberOfUnits
+                };
+
+                return obj;
+            }
+            else
+            {
+                throw new Exception("Products have different Product Name");
+            }
+        }
+
+        public static Bolt SubtractingUnitsOfProduction(Bolt product, int numberOfUnits)
+        {
+            var obj = new Bolt("", 0, 0, 0)
+            {
+                ProductName = product.ProductName,
+                PurchaseCost = product.PurchaseCost,
+                Margin = product.Margin
             };
+
+            if (product.NumberOfUnits >= numberOfUnits)
+            {
+                obj.NumberOfUnits = product.NumberOfUnits - numberOfUnits;
+            }
+            else
+            {
+                throw new Exception("Cannot subtract more units than there are in the product.");
+            }
+            
+            return obj;
+        }
+
+        public static Brick SubtractingUnitsOfProduction(Brick product, int numberOfUnits)
+        {
+            var obj = new Brick("", 0, 0, 0)
+            {
+                ProductName = product.ProductName,
+                PurchaseCost = product.PurchaseCost,
+                Margin = product.Margin
+            };
+
+            if (product.NumberOfUnits >= numberOfUnits)
+            {
+                obj.NumberOfUnits = product.NumberOfUnits - numberOfUnits;
+            }
+            else
+            {
+                throw new Exception("Cannot subtract more units than there are in the product.");
+            }
+
+            return obj;
+        }
+
+        public static Screw SubtractingUnitsOfProduction(Screw product, int numberOfUnits)
+        {
+            var obj = new Screw("", 0, 0, 0)
+            {
+                ProductName = product.ProductName,
+                PurchaseCost = product.PurchaseCost,
+                Margin = product.Margin,
+            };
+
+            if (product.NumberOfUnits >= numberOfUnits)
+            {
+                obj.NumberOfUnits = product.NumberOfUnits - numberOfUnits;
+            }
+            else
+            {
+                throw new Exception("Cannot subtract more units than there are in the product.");
+            }
 
             return obj;
         }
