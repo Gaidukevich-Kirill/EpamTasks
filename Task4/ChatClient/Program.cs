8ae7f53e2a07262dc6ab1bb40eda5fa32a65b64d
Kirill Gaidukevich <gaidukevich.kirill@gmail.com>
2022-09-12 00:15:49 +0300

    added chatClient and chatServer

---
+++ Task4/ChatClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatClient
+{
+    internal class Program
+    {
+        static string userName;
+        private const string address = "127.0.0.1";
+        private const int port = 8888;
+
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            Socket socket = new Socket(
+                AddressFamily.InterNetwork, 
+                SocketType.Stream, 
+                ProtocolType.Tcp);
+
+            socket.Connect(ipEndPoint);
+        }
+    }
+}
---
+++ Task4/ChatServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatServer
+{
+    internal class Program
+    {
+        private const int port = 8888;
+        private const string address = "127.0.0.1";
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(TranslitDictionary.ConvertToLatin("Проверяю чтоб работало"));
+
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(ipEndPoint);
+            socket.Listen(5);
+
+            try
+            {
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+
+            socket.Close();
+        }
+    }
+}
