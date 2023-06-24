0a3c878f071061db6a26da75913ba7bab0f3fd25
Kiryl <93928573+Gaidukevich-Kirill@users.noreply.github.com>
2023-06-24 09:19:16 +0300

    Task 5 to Master (#6)
    * added tree class, tree node class, balance method
    
    * work version with class Student
    
    * Added tree balancing function, serialize in xml file

---
+++ Task5/Task5/FileManagment/ITreeXmlWorker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5.FileManagment
+{
+    public interface ITreeXmlWorker
+    {
+        void SaveTreeFileXml(string path, Tree<Student> tree);
+    }
+}
